780623eeb1757cc49dc5677e24c025351fd4db14
Frederico Leonardo <fredleuz@yahoo.com.br>
2022-11-27 17:47:51 -0300

    Adicionando o Fomulário

---
+++ src/pages/Form/index.js
@@ -0,0 +1,122 @@
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+
+import { FormContainer } from "./styled";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    name: yup.string().required("O nome é obrigatório"),
+    telephone: yup.string().min(11,"Digite um telefone válido com DDD").required("O telefone é obrigatório"),
+    email: yup.string().email("Digite um e-mail válido").required("O e-mail é obrigatório"),
+  })
+  .required();
+
+export default function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+
+  function onSubmit(userData) {
+    alert("Cadastro realizado com sucesso")
+    
+     
+  }
+
+  return (
+    <FormContainer>
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1 className="text-white text-center fw-bold my-5 ">
+              Como a sua doação faz a diferença
+            </h1>
+            <h2 className="text-white text-center fw-bold my-5 ">
+              Detalhes do contato
+            </h2>
+            <div className="col">
+              <p className="text-white text-center my-5">
+                Por favor, preencha seus dados para que possamos enviar
+                <br /> todas as informações sobre as doações.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center my-3 ">
+          <div className="col-8 ">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className=" text-center">
+                <label
+                  htmlFor="nome"
+                  className="col-form-label text-white fw-bold "
+                >
+                  Nome:
+                </label>
+                <input
+                  {...register("name", { required: true })}
+                  type="text"
+                  id="name"
+                  className="form-control border-input text-center"
+                  placeholder="Digite seu nome completo"
+                />
+                  <span className=""> {errors.name?.message}</span>
+              </div>
+              <div className="row justify-content-center my-5">
+                <div className="col-5">
+                  <div className=" text-center">
+                    <label
+                      htmlFor="telefone"
+                      className="col-form-label text-white fw-bold "
+                    >
+                      Telefone:
+                    </label>
+                    <input
+                      {...register("telephone", { required: true })}
+                      type="tel"
+                      id="telefone"
+                      className="form-control border-input text-center"
+                      placeholder="Digite seu telefone com DDD"
+                    />
+                      <span className=""> {errors.telephone?.message}</span>
+                  </div>
+                </div>
+                <div className="col-5">
+                  <div className=" text-center">
+                    <label
+                      htmlFor="email"
+                      className="col-form-label text-white fw-bold "
+                    >
+                      E-mail:
+                    </label>
+                    <input
+                      {...register("email", { required: true })}
+                      type="email"
+                      id="email"
+                      className="form-control border-input text-center"
+                      placeholder="Digite seu e-mail completo"
+                    />
+                      <span className=""> {errors.email?.message}</span>
+                  </div>
+                </div>
+              </div>
+              <div className="col text-center my-5">
+                <button className="btn btn-success px-4" type="submit">
+                  Inscrever
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </FormContainer>
+  );
+}
---
+++ src/pages/Form/styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const FormContainer = styled.div`
+  background-color: #254330;
+
+  .border-input {
+    border-radius: 46px;
+    height: 66px;
+    width: 100%;
+  }
+
+  input::placeholder {
+    color: #000000;
+    font-size: 15px;
+    padding: 15px;
+  }
+
+  span {
+    color: #dab927;
+  
+  }
+`;
+
